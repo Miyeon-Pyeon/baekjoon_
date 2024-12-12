de518ff166706896e0201ad89cd5eff6eb022571
Miyeon-Pyeon <90016934+Miyeon-Pyeon@users.noreply.github.com>
2024-12-12 15:23:36 +0900

    [Bronze IV] Title: 영수증, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+X = int(input())
+N = int(input())
+
+sum = 0
+
+for i in range(1, N+1):
+  a, b = map(int, input().split())
+  sum += a*b
+
+if sum == X:
+  print('Yes')
+elif sum != X:
+  print('No')
